6dbeb4a7c135ed36719b9149db7f579a42a01a39
bgvmysore <bgvmysore@gmail.com>
2021-04-09 20:43:50 +0530

    implemented 1st solver

---
+++ 3_Implementation/inc/odeSolvers.h
@@ -0,0 +1,26 @@
+#ifndef ODESOLVERS_H
+#define ODESOLVERS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct OdeSolverObject
+{
+    double m_t0;
+    double m_tend;
+    double m_dt;
+    int Npoints;
+    double* timeArr;
+    double* solArr;
+    double initState;
+
+}OdeSolverObject;
+
+void solverInit(OdeSolverObject* _solObj, double _t0, double _tend, double _dt, double _initState);
+
+void solverFree(OdeSolverObject* _solObj);
+
+void solverSolve(OdeSolverObject* _solObj, void const* _ODE_Obj, 
+double (*callStateEqofODE)(void const* _folode_obj, double _t, double _prevState));
+
+#endif // ODESOLVER_H
