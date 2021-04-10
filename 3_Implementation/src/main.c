c54df663c0223153410f460c2cf206ee0239ae52
bgvmysore <bgvmysore@gmail.com>
2021-04-10 22:17:55 +0530

    testing new things

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -14,13 +14,15 @@ int main(int argn, char** argv){
 
 	solverSolveRungeKutta4(&solve_rl, &RL_ckt, FOLODE_callStateEquation);
 
-	gnuplot_ctrl *h1;
-	h1 = gnuplot_init();
-	gnuplot_resetplot(h1);
-	gnuplot_setstyle(h1, "lines");
-	gnuplot_plot_xy(h1, solve_rl.timeArr, solve_rl.solArr, solve_rl.Npoints, "RL CKT");
-	getchar();
-	gnuplot_close(h1);
+	saveSolToCsv(&solve_rl);
+
+	// gnuplot_ctrl *h1;
+	// h1 = gnuplot_init();
+	// gnuplot_resetplot(h1);
+	// gnuplot_setstyle(h1, "lines");
+	// gnuplot_plot_xy(h1, solve_rl.timeArr, solve_rl.solArr, solve_rl.Npoints, "RL CKT");
+	// getchar();
+	// gnuplot_close(h1);
 
 	solverFree(&solve_rl);
 	return 0;
