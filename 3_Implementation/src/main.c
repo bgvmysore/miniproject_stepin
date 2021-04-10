29e079eacadb157c3fcd19120e39d946abf7c05c
bgvmysore <bgvmysore@gmail.com>
2021-04-10 21:39:39 +0530

    white space fix

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -7,21 +7,21 @@
 
 int main(int argn, char** argv){
 
-		FirstOrderLinearODE RL_ckt;
-		OdeSolverObject solve_rl; 
+	FirstOrderLinearODE RL_ckt;
+	OdeSolverObject solve_rl; 
 
-		fileToFOLODEnSolObj("inputData.txt", &RL_ckt, &solve_rl);
+	fileToFOLODEnSolObj("inputData.txt", &RL_ckt, &solve_rl);
 
-		solverSolveRungeKutta4(&solve_rl, &RL_ckt, FOLODE_callStateEquation);
+	solverSolveRungeKutta4(&solve_rl, &RL_ckt, FOLODE_callStateEquation);
 
-		gnuplot_ctrl *h1;
-		h1 = gnuplot_init();
-		gnuplot_resetplot(h1);
-		gnuplot_setstyle(h1, "lines");
-		gnuplot_plot_xy(h1, solve_rl.timeArr, solve_rl.solArr, solve_rl.Npoints, "RL CKT");
-		getchar();
-		gnuplot_close(h1);
+	gnuplot_ctrl *h1;
+	h1 = gnuplot_init();
+	gnuplot_resetplot(h1);
+	gnuplot_setstyle(h1, "lines");
+	gnuplot_plot_xy(h1, solve_rl.timeArr, solve_rl.solArr, solve_rl.Npoints, "RL CKT");
+	getchar();
+	gnuplot_close(h1);
 
-		solverFree(&solve_rl);
-		return 0;
+	solverFree(&solve_rl);
+	return 0;
 }
