23479213f09cdfa6d24c3103a728f924781e4310
bgvmysore <bgvmysore@gmail.com>
2021-04-12 21:22:13 +0530

    all modules done

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -11,18 +11,11 @@ int main(int argn, char** argv){
 	OdeSolverObject solve_rl; 
 
 	fileToFOLODEnSolObj("inputData.txt", &RL_ckt, &solve_rl);
-
 	solverSolveRungeKutta4(&solve_rl, &RL_ckt, FOLODE_callStateEquation);
-
 	saveSolToCsv(&solve_rl);
 
-	// gnuplot_ctrl *h1;
-	// h1 = gnuplot_init();
-	// gnuplot_resetplot(h1);
-	// gnuplot_setstyle(h1, "lines");
-	// gnuplot_plot_xy(h1, solve_rl.timeArr, solve_rl.solArr, solve_rl.Npoints, "RL CKT");
-	// getchar();
-	// gnuplot_close(h1);
+	saveSolToCsv(&RL_ckt);
+	dispSolAsPlot(&RL_ckt);
 
 	solverFree(&solve_rl);
 	return 0;
