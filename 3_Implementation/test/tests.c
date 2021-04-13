a91fed7f89a3a5af190142dfaa90f6de3ed5f084
bgvmysore <bgvmysore@gmail.com>
2021-04-13 18:24:00 +0530

    test file

--- 3_Implementation/test/tests.c
+++ 3_Implementation/test/tests.c
@@ -1,7 +1,7 @@
 #include "unity.h"
 
-void setUp(void){}
-void tearDown(void){}
+void setUp(void){ return ; }
+void tearDown(void){ return ; }
 
 void test_test(void){
     TEST_ASSERT_EQUAL_INT(1,1);
