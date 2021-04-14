5a890dbbeb8e6cda9c0ba2453ec379dededf6482
bgvmysore-lp <bgvmysore@gmail.com>
2021-04-14 16:55:06 +0530

    std 11 and doc comm

--- 3_Implementation/inc/FirstOrderLinearODE.h
+++ 3_Implementation/inc/FirstOrderLinearODE.h
@@ -5,6 +5,10 @@
 #include<stdio.h>
 #include<math.h>
 
+/**
+ * @brief This struct is used to store all the data related to First Order Linear Differential Equation.
+ * x'(t) = A * x(t)  + B * u(t - s) with initial value x0 at t0
+ */
 typedef struct _FOLODE{
     double m_A;
     double m_B;
@@ -14,6 +18,18 @@ typedef struct _FOLODE{
     double m_ip_freq;
 }FirstOrderLinearODE;
 
+/**
+ * @brief This function is used to initialize the first order linear ODE object.
+ * x'(t) = A * x(t)  + B * u(t - s) with initial value x0 at t0
+ * 
+ * @param _folode_obj (FirstOrderLinearODE *) pointer to first order linear ODE object
+ * @param _A (double) Value of A in ODE
+ * @param _B (double) Value of B in ODE
+ * @param _input_funct ( function pointer ) Input function for the ODE
+ * @param _ipTimeDelay (double) time shift of the input function
+ * @param _init_state (double) Initial state of state variable x at t0
+ * @param _freq (double) frequency of input function if input function is sin
+ */
 void FOLODE_init(
                     FirstOrderLinearODE * _folode_obj, 
                     double _A, 
@@ -24,6 +40,13 @@ void FOLODE_init(
                     double _freq
                 );
 
+/**
+ * @brief 
+ * 
+ * @param _folode_obj 
+ * @param _t 
+ * @return double 
+ */
 double FOLODE_callInputFunct(FirstOrderLinearODE const* _folode_obj, double _t);
 
 double FOLODE_callStateEquation(void const* _folode_obj, double _t, double _prevState);
